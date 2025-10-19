a674ae6c087a17b1ef8cdd151d645509704b37de
I'm Ranson <imranson@Ransons-MacBook-Jesus-Pro.local>
2025-10-19 12:08:25 +0100

    Add files for csf

---
+++ batch_jobs/jobscript-1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+### Choose ONE of the following partitions depending on your permitted access
+
+#SBATCH -p gpuA              
+### Required flags
+#SBATCH -G 1                 # (or --gpus=N) Number of GPUs 
+#SBATCH -t 4-0               # Wallclock timelimit (1-0 is one day, 4-0 is max permitted)
+
+### Optional flags
+#SBATCH -n 8          # (or --ntasks=) Number of CPU (host) cores (default is 1)
+                             # See above for number of cores per GPU you can request.
+                             # Also affects host RAM allocated to job unless --mem=num used.
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chun.tham@student.manchester.ac.uk
+
+module purge
+
+conda deactivate
+
+conda activate semantic-decoding
+
+echo "Job is using $SLURM_GPUS GPU(s) with ID(s) $CUDA_VISIBLE_DEVICES and $SLURM_NTASKS CPU core(s)"
+
+conda deactivate
