ea7169ce3c406bbd130afb480db5a542bda85985
I'm Ranson <imranson@Ransons-MacBook-Jesus-Pro.local>
2026-03-06 21:50:26 +0000

    pm test with diff wrmodel jobscriopt

--- batch_jobs/jobscript-15-pm-wrmodel.sh
+++ batch_jobs/jobscript-15-pm-wrmodel.sh
@@ -25,18 +25,6 @@ echo "Job is using $SLURM_GPUS GPU(s) with ID(s) $CUDA_VISIBLE_DEVICES and $SLUR
 python3 -c "import torch;
 print(f'torch cuade is available: {torch.cuda.is_available()}')"
 
-python3 decoding/run_decoder.py --subject S1 --word-rate-voxels speech --experiment perceived_movie --task sintel --tag wr_speech
-python3 decoding/run_decoder.py --subject S1 --word-rate-voxels speech --experiment perceived_movie --task presto --tag wr_speech
-python3 decoding/run_decoder.py --subject S1 --word-rate-voxels speech --experiment perceived_movie --task partlycloudy --tag wr_speech
-python3 decoding/run_decoder.py --subject S1 --word-rate-voxels speech --experiment perceived_movie --task laluna --tag wr_speech
-python3 decoding/run_decoder.py --subject S2 --word-rate-voxels speech --experiment perceived_movie --task sintel --tag wr_speech
-python3 decoding/run_decoder.py --subject S2 --word-rate-voxels speech --experiment perceived_movie --task presto --tag wr_speech
-python3 decoding/run_decoder.py --subject S2 --word-rate-voxels speech --experiment perceived_movie --task partlycloudy --tag wr_speech
-python3 decoding/run_decoder.py --subject S2 --word-rate-voxels speech --experiment perceived_movie --task laluna --tag wr_speech
-python3 decoding/run_decoder.py --subject S3 --word-rate-voxels speech --experiment perceived_movie --task sintel --tag wr_speech
-python3 decoding/run_decoder.py --subject S3 --word-rate-voxels speech --experiment perceived_movie --task presto --tag wr_speech
-python3 decoding/run_decoder.py --subject S3 --word-rate-voxels speech --experiment perceived_movie --task partlycloudy --tag wr_speech
-python3 decoding/run_decoder.py --subject S3 --word-rate-voxels speech --experiment perceived_movie --task laluna --tag wr_speech
 python3 decoding/run_decoder.py --subject S1 --word-rate-voxels auditory --experiment perceived_movie --task sintel --tag wr_auditory
 python3 decoding/run_decoder.py --subject S1 --word-rate-voxels auditory --experiment perceived_movie --task presto --tag wr_auditory
 python3 decoding/run_decoder.py --subject S1 --word-rate-voxels auditory --experiment perceived_movie --task partlycloudy --tag wr_auditory
@@ -50,18 +38,6 @@ python3 decoding/run_decoder.py --subject S3 --word-rate-voxels auditory --exper
 python3 decoding/run_decoder.py --subject S3 --word-rate-voxels auditory --experiment perceived_movie --task partlycloudy --tag wr_auditory
 python3 decoding/run_decoder.py --subject S3 --word-rate-voxels auditory --experiment perceived_movie --task laluna --tag wr_auditory
 
-python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_speech --task sintel
-python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_speech --task presto
-python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_speech --task partlycloudy
-python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_speech --task laluna
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie  --tag wr_speech --task sintel
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie  --tag wr_speech --task presto
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie  --tag wr_speech --task partlycloudy
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie  --tag wr_speech --task laluna
-python3 decoding/evaluate_predictions.py --null 200 --subject S3 --experiment perceived_movie  --tag wr_speech --task sintel
-python3 decoding/evaluate_predictions.py --null 200 --subject S3 --experiment perceived_movie  --tag wr_speech --task presto
-python3 decoding/evaluate_predictions.py --null 200 --subject S3 --experiment perceived_movie  --tag wr_speech --task partlycloudy
-python3 decoding/evaluate_predictions.py --null 200 --subject S3 --experiment perceived_movie  --tag wr_speech --task laluna
 python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_auditory --task sintel
 python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_auditory --task presto
 python3 decoding/evaluate_predictions.py --null 200 --subject S1 --experiment perceived_movie  --tag wr_auditory --task partlycloudy
