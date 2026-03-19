8a476299cba94a49a720520cfd1a07506f097d1c
I'm Ranson <imranson@Ransons-MacBook-Jesus-Pro.local>
2026-03-19 21:17:56 +0000

    test train_em code with multigptlayer functionality

---
+++ batch_jobs/jobscript-20-gpt-layers-train.sh
@@ -0,0 +1,37 @@
+#!/bin/bash --login
+### Choose ONE of the following partitions depending on your permitted access
+
+#SBATCH -p gpuL         
+### Required flags
+#SBATCH -G 1                 # (or --gpus=N) Number of GPUs 
+#SBATCH -t 4-0               # Wallclock timelimit (1-0 is one day, 4-0 is max permitted)
+
+### Optional flags
+#SBATCH -n 8          # (or --ntasks=) Number of CPU (host) cores (default is 1)
+                             # See above for number of cores per GPU you can request.
+                             # Also affects host RAM allocated to job unless --mem=num used.
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=chun.tham@student.manchester.ac.uk
+
+module purge
+conda deactivate
+
+module load libs/cuda
+conda activate semantic-decoding
+
+echo "Job is using $SLURM_GPUS GPU(s) with ID(s) $CUDA_VISIBLE_DEVICES and $SLURM_NTASKS CPU core(s)"
+
+python3 -c "import torch;
+print(f'torch cuda is available: {torch.cuda.is_available()}')"
+
+python3 decoding/train_EM.py --subject S1 --gpt perceived --gpt-layers 3 5 7 9
+python3 decoding/train_EM.py --subject S1 --gpt imagined --gpt-layers 3 5 7 9
+python3 decoding/train_EM.py --subject S2 --gpt perceived --gpt-layers 3 5 7 9
+python3 decoding/train_EM.py --subject S2 --gpt imagined --gpt-layers 3 5 7 9
+python3 decoding/train_EM.py --subject S3 --gpt perceived --gpt-layers 3 5 7 9
+python3 decoding/train_EM.py --subject S3 --gpt imagined --gpt-layers 3 5 7 9
+
+module purge
+conda deactivate
+
