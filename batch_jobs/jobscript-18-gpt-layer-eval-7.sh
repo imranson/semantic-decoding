d491511fe3f319f84be2e5f9db3ab66ef8567f17
I'm Ranson <imranson@Ransons-MacBook-Jesus-Pro.local>
2026-03-23 21:55:55 +0000

    edit scripts to rerun failed jobs

--- batch_jobs/jobscript-18-gpt-layer-eval-7.sh
+++ batch_jobs/jobscript-18-gpt-layer-eval-7.sh
@@ -26,37 +26,37 @@ python3 -c "import torch;
 print(f'torch cuda is available: {torch.cuda.is_available()}')"
 
 python3 decoding/run_decoder.py --subject S2 --experiment perceived_speech --task wheretheressmoke --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment perceived_movie --task presto --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment perceived_movie --task partlycloudy --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment perceived_movie --task laluna --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment perceived_movie --task presto --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment perceived_movie --task partlycloudy --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment perceived_movie --task laluna --gpt-layer 7 --tag gpt_layer_7
 python3 decoding/run_decoder.py --subject S2 --experiment perceived_multispeaker --task attend-M --gpt-layer 7 --tag gpt_layer_7
 python3 decoding/run_decoder.py --subject S2 --experiment perceived_multispeaker --task attend-F --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task alpha_repeat-1 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task alpha_repeat-2 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task bravo_repeat-1 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task bravo_repeat-2 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task charlie_repeat-1 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task charlie_repeat-2 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task delta_repeat-1 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task delta_repeat-2 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task echo_repeat-1 --gpt-layer 7 --tag gpt_layer_7
-python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task echo_repeat-2 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task alpha_repeat-1 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task alpha_repeat-2 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task bravo_repeat-1 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task bravo_repeat-2 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task charlie_repeat-1 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task charlie_repeat-2 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task delta_repeat-1 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task delta_repeat-2 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task echo_repeat-1 --gpt-layer 7 --tag gpt_layer_7
+# python3 decoding/run_decoder.py --subject S2 --experiment imagined_speech --task echo_repeat-2 --gpt-layer 7 --tag gpt_layer_7
 python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_speech --task wheretheressmoke --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie --task presto --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie --task partlycloudy --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie --task laluna --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie --task presto --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie --task partlycloudy --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_movie --task laluna --tag gpt_layer_7
 python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_multispeaker --task attend-M --tag gpt_layer_7
 python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment perceived_multispeaker --task attend-F --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task alpha_repeat-1 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task alpha_repeat-2 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task bravo_repeat-1 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task bravo_repeat-2 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task charlie_repeat-1 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task charlie_repeat-2 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task delta_repeat-1 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task delta_repeat-2 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task echo_repeat-1 --tag gpt_layer_7
-python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task echo_repeat-2 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task alpha_repeat-1 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task alpha_repeat-2 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task bravo_repeat-1 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task bravo_repeat-2 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task charlie_repeat-1 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task charlie_repeat-2 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task delta_repeat-1 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task delta_repeat-2 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task echo_repeat-1 --tag gpt_layer_7
+# python3 decoding/evaluate_predictions.py --null 200 --subject S2 --experiment imagined_speech --task echo_repeat-2 --tag gpt_layer_7
 
 python3 decoding/run_decoder.py --subject S3 --experiment perceived_speech --task wheretheressmoke --gpt-layer 7 --tag gpt_layer_7
 python3 decoding/run_decoder.py --subject S3 --experiment perceived_movie --task presto --gpt-layer 7 --tag gpt_layer_7
